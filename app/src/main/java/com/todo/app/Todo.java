81848d3907320d459d1da6afd7362fe6d75b2bff
Marc Fernandez <marc.fernandezg@gmail.com>
2014-05-14 22:30:50 -0700

    Initial commit

---
+++ app/src/main/java/com/todo/app/Todo.java
@@ -0,0 +1,15 @@
+package com.todo.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Todo extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_todo);
+  }
+}
